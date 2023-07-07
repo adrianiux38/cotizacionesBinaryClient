83f5473181089156758fc801885b6e4963c4c21f
Sticky <adrian.gutierrez@techsoftconsulting.mx>
2023-07-07 13:21:28 -0600

    listo crear servicio

--- src/components/CreateService.jsx
+++ src/components/CreateService.jsx
@@ -3,71 +3,94 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import NavBar from './NavBar';
 import {React, useState} from 'react';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import { NumericFormat } from 'react-number-format';
+import { useNavigate } from 'react-router-dom';
 
 const CreateService = () => {
+  const navigate = useNavigate();
+  const [newService, setNewService] = useState({
+    nombre: "",
+    descripcion: "",
+    precio: 0,
+    esquema: ""
+  });
 
-    
-    const [newService, setNewService] = useState({
-        nombre: "",
-        descripcion: "",
-        precio: 0,
-        esquema: ""
-      });
+  const handleChangeDescription = e => {
+    setNewService({
+      ...newService,
+      descripcion: e.target.value
+    });
+  };
 
-      const handleChangeDescription = e => {
-        setNewService({
-          ...newService,
-          descripcion: e.target.value
-        });
-      };
+  const handleChangeName = e => {
+    setNewService({
+      ...newService,
+      nombre: e.target.value
+    });
+  };
 
-      const handleChangeName = e => {
-        setNewService({
-          ...newService,
-          nombre: e.target.value
-        });
-      };
-      const handleChangePrice = e => {
-        setNewService({
-          ...newService,
-          precio: e.target.value
-        });
-      };
-    
-      const handleChangePaymentPlan = e => {
-        setNewService({
-          ...newService,
-          esquema: e.target.value
-        });
-      };
+  const handleChangePrice = values => {
+    const {floatValue} = values;
+    setNewService({
+      ...newService,
+      precio: floatValue
+    });
+  };
 
-      const handleAddService = async () => {
-        await fetch("https://binaryquotations.herokuapp.com/createService", {
-           method: "POST",
-           headers: {
-             "Content-Type": "application/json",
-           },
-           body: JSON.stringify({
-             nombre: newService.nombre,
-             descripcion: newService.descripcion,
-             precio: String(newService.precio),
-             esquema: newService.esquema
-             }),
-         })
-           .then((res) => res.json())
-           .then((data) => {
-             console.log(data);
-           })
-           .catch(err => console.log(err))
-       };
+  const handleChangePaymentPlan = e => {
+    setNewService({
+      ...newService,
+      esquema: e.target.value
+    });
+  };
 
-      
+  const handleAddService = async () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+      const response = await fetch("http://localhost:3001/createService", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nombre: newService.nombre,
+          descripcion: newService.descripcion,
+          precio: newService.precio,
+          esquema: newService.esquema
+        }),
+      })
+      if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`);
+      }
+      const data = await response.json();
+      if (data.success) {
+          alert(`Service created successfully`);
+      } else {
+          console.error(
+          "Error creating service",
+          data.error
+          );
+          reject()
+      }
+      } catch (error) {
+          reject()
+
+      }
+      navigate('/');
+      resolve();
+    });
+  };
+
   return (
-     <div>
-        <NavBar/>
-        <h2 id="addService">Agregar nuevo servicio</h2>
-          <div style={{ width:'100vw', display: 'flex', flexDirection:'column', justifyContent:'center', alignContent:'center' }}>
-          <Box
+    <div>
+      <NavBar/>
+      <h2 id="addService">Agregar nuevo servicio</h2>
+      <div style={{ width:'100vw', display: 'flex', flexDirection:'column', justifyContent:'center', alignItems:'center' }}>
+        <Box
           component="form"
           sx={{
             '& > :not(style)': { m: 1, width: '25ch'},
@@ -79,34 +102,44 @@ const CreateService = () => {
             id="nombre"
             label="nombre"
             name='nombre'
-            value={newService.nombre || ""}
+            value={newService.nombre || ""}
             onChange={handleChangeName}
           />
           <TextField
             id="descripcion"
             label="descripcion"
             name='descripcion'
-            value={newService.descripcion || ""}
+            value={newService.descripcion || ""}
             onChange={handleChangeDescription}
           />
-          <TextField
+          <NumericFormat
+            customInput={TextField}
             id="precio"
-            label="precio"
-            name='precio'
-            value={newService.precio || ""}
-            onChange={handleChangePrice}
+            label="Precio"
+            value={newService.precio || ''}
+            onValueChange={(values) => handleChangePrice(values)}
+            thousandSeparator={true}
+            decimalScale={3}
+            prefix={'$'}
+            isNumericString={true}
           />
-          <TextField
-            id="esquema"
-            label="esquema"
-            name='esquema'
-            value={newService.esquema || ""}
-            onChange={handleChangePaymentPlan}
-          />
+          <FormControl fullWidth variant="standard">
+            <InputLabel id="esquema">Esquema</InputLabel>
+            <Select
+              labelId="esquema"
+              id="esquema"
+              value={newService.esquema}
+              label="Esquema"
+              onChange={handleChangePaymentPlan}
+            >
+              <MenuItem value="pago_fijo">Pago Fijo</MenuItem>
+              <MenuItem value="pago_mensual">Pago Mensual</MenuItem>
+            </Select>
+          </FormControl>
         </Box>
         <Button variant="contained" id="addServiceButton" onClick={handleAddService}>Agregar servicio</Button>
-        </div>
-        </div>
+      </div>
+    </div>
   )
 }
 
